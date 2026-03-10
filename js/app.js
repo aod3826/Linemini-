594177e340b112bf5a3f761f3aae66b7f9651704
aod3826 <pantipa3826@gmail.com>
2026-03-10 17:43:08 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 // ===== CONFIG =====
 // Replace these with your actual values
 const APP_CONFIG = {
-  LIFF_ID: 'YOUR_LIFF_ID',                // From LINE Developers Console
+  LIFF_ID: '2009141036-nx2nIzhS',                // From LINE Developers Console
   GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbzuhLRc44K-ZREA5NZ4M_se_j_vDS4YgM8SFojvTsOAA7BaMLGds120QZym126jgkcX/exec',
   ADMIN_KEY:    'aod',      // Change to a random string
   LINE_PAY_ENV: 'sandbox',                 // 'sandbox' or 'production'
